f34b5e19b3e99f442ee2c19949dc9fa37405c51a
김정기 <kim.jeongki80@navercorp.com>
2020-04-15 18:03:46 +0900

    env: webpack.config.js 에  babel targets 설정을 포함한 config 반영함

--- build-utils/webpack.production.js
+++ build-utils/webpack.production.js
@@ -1,25 +1,12 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = () => ({
-  output: {
-    // Make sure to use [name] or [id] in output.filename
-    //  when using multiple entry points
-    filename: "[name].bundle.js",
-    chunkFilename: "[id].bundle.js"
-},
   module: {
     rules: [
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
-      {
-        test: /\.(ts|js)$/, // js 확장자 추가
-        exclude: /node_modules\/(?!(@webcomponents\/shadycss|lit-element|lit-html)\/).*/,
-        use: {
-          loader: 'babel-loader'
-        }
-      }
     ],
   },
   optimization: {
